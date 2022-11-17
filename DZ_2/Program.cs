34e1d74e5095db937c057a51b6e3a9c97cf6b8c9
Anton <kuleshov.event@mail.ru>
2022-11-18 00:39:55 +0400

    Решение второй задачи

--- DZ_2/Program.cs
+++ DZ_2/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, 
+//которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.WriteLine("Введите первое число");
+int numberFirst = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int numberSecond = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int numberThird = Convert.ToInt32(Console.ReadLine());
+if (numberFirst > numberSecond)
+{
+    if (numberFirst > numberThird)
+    {
+        Console.WriteLine("Number First=max");
+    }
+Console.WriteLine("Number First=max");
+}
+else if (numberFirst < numberSecond)
+{
+    if ( numberSecond > numberThird)
+    {
+        Console.WriteLine("Number Second=max");
+    }
+Console.WriteLine("Number Second=max");
+}
+else if (numberThird > numberSecond)
+{
+    if (numberThird > numberFirst)
+    {
+        Console.WriteLine("Number Third=max");
+    }
+Console.WriteLine("Number Third=max");
+}
