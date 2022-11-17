892c6d119ca6eca69c8880de08c07af59282d3ae
Anton <kuleshov.event@mail.ru>
2022-11-17 23:55:51 +0400

    Первое домашнее задание

---
+++ DZ_1/Program.cs
@@ -0,0 +1,14 @@
+﻿//Решение первой задачи. Напишите программу которая на вход принимает два числа и 
+//выдает, какое больше и какое меньше 
+Console.WriteLine("Введите первое число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if (numberA < numberB)
+{
+Console.WriteLine ("numberSecond=max,numberFirst=min");
+}
+else
+{
+    Console.WriteLine("numberFirst=max,numberSecond=min");
+}
